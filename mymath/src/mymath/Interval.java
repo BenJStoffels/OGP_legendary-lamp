555bf329cdac03dc9bd60a41327ab1083f59dea9
Ben Stoffels <ben.stoffels@pulsar.wina.be>
2024-02-19 09:24:25 +0100

    add Interval

---
+++ mymath/src/mymath/Interval.java
@@ -0,0 +1,18 @@
+package mymath;
+
+public class Interval {
+	private int ondergrens;
+	private int bovengrens;
+	
+	public static int getLengte(Interval interval) {
+		return interval.bovengrens - interval.ondergrens;
+	}
+	
+	public static void setOndergrens(Interval interval, int nieuweOndergrens) {
+		interval.ondergrens = nieuweOndergrens;
+	}
+	
+	public static void setBovengrens(Interval interval, int nieuweBovengrens) {
+		interval.bovengrens = nieuweBovengrens;
+	}
+}
